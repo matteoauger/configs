77026e1035dac4ba2cd216179ef7d6800e7f3dde
Matteo AUGER <33668505+matteoauger@users.noreply.github.com>
2025-09-01 16:33:25 +0200

    Update colorscheme.lua

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,16 +1,12 @@
--- Here goes the colorschemes
-
 return {
     { "rose-pine/neovim", name = "rose-pine" },
-    {
-        "felipeagc/fleet-theme-nvim",
-    },
+    { "felipeagc/fleet-theme-nvim" },
+    { "ellisonleao/gruvbox.nvim" },
+    { "rebelot/kanagawa.nvim" },
     {
         "LazyVim/LazyVim",
         opts = {
-            -- Define your colorscheme here
-            colorscheme = "retrobox",
-            -- colorscheme = "fleet",
+            colorscheme = "kanagawa",
         },
     },
 }
