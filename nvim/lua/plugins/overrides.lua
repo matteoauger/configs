2f32bd5fef6dc06a15caef1a39795bb0f30e6f5b
Matteo AUGER <33668505+matteoauger@users.noreply.github.com>
2025-09-17 09:19:39 +0200

    Update plugin overrides in overrides.lua

--- nvim/lua/plugins/overrides.lua
+++ nvim/lua/plugins/overrides.lua
@@ -1,11 +1,56 @@
 -- Here goes the default plugins overrides
-
 return {
-    {
-        -- Opening neotree in the current window
-        "nvim-neo-tree/neo-tree.nvim",
-        opts = {
-            window = { position = "current" },
-        },
+  --{
+  -- Opening neotree in the current window
+  --"nvim-neo-tree/neo-tree.nvim",
+  --opts = {
+  --  window = { position = "current" },
+  --},
+  --},
+  {
+    "seblyng/roslyn.nvim",
+    ---@module 'roslyn.config'
+    ---type RoslynNvimConfig
+    opts = {},
+  },
+  {
+    "williamboman/mason.nvim",
+
+    dependencies = {
+      "williamboman/mason-lspconfig.nvim",
     },
+
+    config = function()
+      require("mason").setup({
+        registries = {
+          "github:mason-org/mason-registry",
+          "github:Crashdummyy/mason-registry",
+        },
+        ensure_installed = {
+          "roslyn",
+          "csharpier",
+          "netcorebg",
+        },
+      })
+    end,
+  },
+
+  -- Snacks explorer
+  {
+    "folke/snacks.nvim",
+    opts = {
+      explorer = {
+        -- your explorer configuration comes here
+        -- or leave it empty to use the default settings
+        -- refer to the configuration section below
+      },
+      picker = {
+        sources = {
+          explorer = {
+            layout = { preset = "default", preview = true },
+          },
+        },
+      },
+    },
+  },
 }
