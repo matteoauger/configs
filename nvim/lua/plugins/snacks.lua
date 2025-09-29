424b355f884822c25a7b1c3c863feb8c6ca5006e
Matteo AUGER <matteo.auger@protonmail.com>
2025-09-29 17:03:39 +0200

    Update neovim config

---
+++ nvim/lua/plugins/snacks.lua
@@ -0,0 +1,44 @@
+return {
+    {
+        "folke/snacks.nvim",
+        opts = {
+            explorer = {
+                -- your explorer configuration comes here
+                -- or leave it empty to use the default settings
+                -- refer to the configuration section below
+            },
+            picker = {
+                sources = {
+                    explorer = {
+                        layout = { preset = "default", preview = true },
+                        auto_close = true,
+                    },
+                },
+            },
+            dashboard = {
+                preset = {
+                    header = [[
+       ⣴⣶⣤⡤⠦⣤⣀⣤⠆     ⣈⣭⣭⣿⣶⣿⣦⣼⣆
+        ⠉⠻⢿⣿⠿⣿⣿⣶⣦⠤⠄⡠⢾⣿⣿⡿⠋⠉⠉⠻⣿⣿⡛⣦
+              ⠈⢿⣿⣟⠦ ⣾⣿⣿⣷⠄⠄⠄⠄⠻⠿⢿⣿⣧⣄
+               ⣸⣿⣿⢧ ⢻⠻⣿⣿⣷⣄⣀⠄⠢⣀⡀⠈⠙⠿⠄
+              ⢠⣿⣿⣿⠈  ⠡⠌⣻⣿⣿⣿⣿⣿⣿⣿⣛⣳⣤⣀⣀
+       ⢠⣧⣶⣥⡤⢄ ⣸⣿⣿⠘⠄ ⢀⣴⣿⣿⡿⠛⣿⣿⣧⠈⢿⠿⠟⠛⠻⠿⠄
+      ⣰⣿⣿⠛⠻⣿⣿⡦⢹⣿⣷   ⢊⣿⣿⡏  ⢸⣿⣿⡇ ⢀⣠⣄⣾⠄
+     ⣠⣿⠿⠛⠄⢀⣿⣿⣷⠘⢿⣿⣦⡀ ⢸⢿⣿⣿⣄ ⣸⣿⣿⡇⣪⣿⡿⠿⣿⣷⡄
+     ⠙⠃   ⣼⣿⡟  ⠈⠻⣿⣿⣦⣌⡇⠻⣿⣿⣷⣿⣿⣿ ⣿⣿⡇⠄⠛⠻⢷⣄
+          ⢻⣿⣿⣄   ⠈⠻⣿⣿⣿⣷⣿⣿⣿⣿⣿⡟ ⠫⢿⣿⡆
+           ⠻⣿⣿⣿⣿⣶⣶⣾⣿⣿⣿⣿⣿⣿⣿⣿⡟⢀⣀⣤⣾⡿⠃     ]],
+                },
+
+                sections = {
+                    { section = "header" },
+                    { section = "keys",   gap = 1, padding = 1 },
+                    { section = "startup" },
+                },
+            }
+        },
+    },
+
+}
+
